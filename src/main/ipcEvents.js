190d25ac25323305f51604b8ed04cd239ccd67dd
Sergio Peris <dev.sertxu@gmail.com>
2019-02-19 09:40:38 +0100

    Modified ipc Events

--- src/main/ipcEvents.js
+++ src/main/ipcEvents.js
@@ -1,59 +1,8 @@
-const ipc = require('electron').ipcMain
-
-
 export default () => {
-
   require('./ipcEvents/songs').default()
   require('./ipcEvents/artists').default()
   require('./ipcEvents/albums').default()
 
   require('./ipcEvents/search').default()
-
-  // ipc.on('album-tracks', async function (event, request) {
-  //   let response
-  //
-  //   switch (request.method) {
-  //     case 'paginate':
-  //       if (!request.page) return null
-  //       if (!request.pageSize) return null
-  //       response = await SongCollection.getWherePagination(request.page, request.pageSize, request.where)
-  //       event.sender.send('album-tracks-pagination', response)
-  //       break
-  //   }
-  // })
-  //
-  // ipc.on('artist-albums', async function (event, request) {
-  //   let response
-  //
-  //   switch (request.method) {
-  //     case 'paginate':
-  //       if (!request.page) return null
-  //       if (!request.pageSize) return null
-  //       response = await SongCollection.getAlbumsPaginate(request.page, request.pageSize, request._id)
-  //       event.sender.send('artist-albums-paginate', response)
-  //       break
-  //   }
-  // })
-  //
-  // ipc.on('populars', async function (event, request) {
-  //
-  // })
-  //
-  //
-  // ipc.on('play-all', async function (event, request) {
-  //   let response
-  //
-  //   switch (request.method) {
-  //     case 'album':
-  //       response = await AlbumCollection.getAllSongs(request._id)
-  //       event.sender.send('add-songs-playlist', response)
-  //       break
-  //
-  //     case 'artist':
-  //       response = await ArtistCollection.getAllSongs(request._id)
-  //       event.sender.send('add-songs-playlist', response)
-  //       break
-  //   }
-  // })
-
+  require('./ipcEvents/history').default()
 }
--- src/main/ipcEvents/albums.js
+++ src/main/ipcEvents/albums.js
@@ -1,6 +1,6 @@
 const ipc = require('electron').ipcMain
 
-const Album = require('../collections/album').default
+const Album = require('../collections/albumCollection').default
 const AlbumCollection = new Album()
 
 export default () => {
@@ -9,16 +9,10 @@ export default () => {
 
     switch (request.method) {
       case 'firstOrCreate':
-        if (!request.data) return null
-        response = await AlbumCollection.firstOrCreate(request.data)
-        if (response) event.returnValue = response
-        break
-
-      case 'show':
         if (!request.where) return null
-        response = await AlbumCollection.find(request.where)
-        console.log(request, response)
-        event.sender.send('albums-show', response)
+        if (!request.insert) return null
+        response = await AlbumCollection.firstOrCreate(request.where, request.insert)
+        if (response) event.returnValue = response
         break
 
       case 'paginate':
@@ -26,6 +20,48 @@ export default () => {
         if (!request.pageSize) return null
         response = await AlbumCollection.paginate(request.page, request.pageSize)
         event.sender.send('albums-paginate', response)
+        break
+
+      case 'show':
+        if (!request.where) return null
+        response = await AlbumCollection.find(request.where)
+        event.sender.send('albums-show', response)
+        break
+
+      case 'songs':
+        if (!request.page) return null
+        if (!request.pageSize) return null
+        if (!request.where) return null
+        response = await AlbumCollection.songs(request.page, request.pageSize, request.where)
+        event.sender.send('albums-songs', response)
+        break
+
+      case 'playAll':
+        if (!request.where) return null
+        response = await AlbumCollection.playAll(request.where)
+        event.sender.send('add-songs-playlist', response)
+        console.log('playAll', response, request)
+        break
+
+      case 'remove':
+        if (!request.where) return null
+        console.log('remove')
+        break
+
+      case 'edit':
+        if (!request.where) return null
+        console.log('edit')
+        break
+
+      case 'getRecommendation':
+        if (!request.where) return null
+        console.log('getRecommendation')
+        break
+
+      case 'addToPlaylist':
+        if (!request.where) return null
+        if (!request.playlist) return null
+        console.log('addToPlaylist')
         break
     }
   })
