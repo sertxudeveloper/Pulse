55e89a33141bbd475152d02097c5d606127bc4a1
Sergio Peris <dev.sertxu@gmail.com>
2019-02-19 09:42:40 +0100

    Changed base collection

--- src/main/collections/_base.js
+++ src/main/collections/_base.js
@@ -1,3 +1,5 @@
+const Mongo = require('../database').default
+
 class Base {
 
   constructor (data) {
@@ -5,57 +7,35 @@ class Base {
     if (global && global.db) this.collection = global.db.collection(this.model)
   }
 
-  async get () {
-    this.collection.createIndex({title: 1})
-    return await this.collection.find({}, {_id: 0}).sort({title: 1}).toArray()
-      .then(result => (result) ? result : null)
-      .catch(error => console.error(error))
+  /**
+   * Gets the first document, if not exists will create it
+   * @param where
+   * @param insert
+   * @returns {Object}
+   */
+  async firstOrCreate (where, insert) {
+    /**
+     * Check if this.collection is defined, if not create a new connection
+     * This conditional is executed if it's requested by the worker
+     */
+    if (!this.collection) {
+      const mongo = new Mongo()
+      const db = await mongo.connect()
+      this.collection = db.collection(this.model)
+    }
+
+    return await this.collection.findOneAndUpdate(
+      where, {$setOnInsert: insert}, {returnNewDocument: true, upsert: true})
   }
 
-  async search (query) {
-    this.collection.createIndex({title: 1})
-    this.collection.createIndex({name: 'text'})
-    return await this.collection.find({
-      // name: {$regex: query, $options: 'i'}
-      $text: {$search: query, $caseSensitive: false, $diacriticSensitive: false}
-    }, {_id: 0}).sort({title: 1}).toArray()
-      .then(result => (result) ? result : null)
-      .catch(error => console.error(error))
-  }
-
-  async paginate (page, pageSize) {
-    // Get all elements
-    let response = {}
-    this.collection.createIndex({title: 1})
-    const cursor = this.collection.find({}, {_id: 0}).sort({title: 1})
-
-    response.data = await cursor.skip(pageSize * (page - 1)).limit(pageSize).toArray()
-      .then(result => (result) ? result : null)
-      .catch(error => console.error(error))
-
-    response.count = await cursor.count()
-    return response
-  }
-
+  /**
+   * Get the requested document
+   * @param where Object with the query
+   * @returns {Object}
+   */
   async find (where) {
     return await this.collection.findOne(where)
   }
-
-  async insert (data) {
-    return await this.collection.insertOne(data)
-      .then(res => res)
-      .catch(error => console.error(error))
-  }
-
-  async firstOrCreate (data) {
-    let find = await this.collection.findOne({title: data._id})
-      .then(result => (result) ? result : null)
-      .catch(error => console.error(error))
-    if (find !== null) return find
-    return await this.collection.insertOne(data)
-      .then(res => res)
-      .catch(error => console.error(error))
-  }
 }
 
-export default Base
+ export default Base
