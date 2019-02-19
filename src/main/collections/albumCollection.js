a427e4134919e9a8764d2fa296f352ec36865b50
Sergio Peris <dev.sertxu@gmail.com>
2019-02-19 09:43:05 +0100

    Modified Album, Artist and Song Collection

--- src/main/collections/albumCollection.js
+++ src/main/collections/albumCollection.js
@@ -1,51 +1,18 @@
 const Base = require('./_base').default
-const Mongo = require('../database').default
 
-class Album extends Base {
+class AlbumCollection extends Base {
   constructor () {
     super({model: 'albums'})
   };
 
-  async firstOrCreate (data) {
-    const mongo = new Mongo()
-    const db = await mongo.connect()
-    this.collection = db.collection(this.model)
-
-    return await this.collection.findOneAndUpdate(
-      {name: data.name},
-      {$setOnInsert: {picture: data.picture}},
-      {returnNewDocument: true, upsert: true})
-  }
-
-  async find (where) {
-    this.collection.createIndex({title: 1})
-
-    const cursor = this.collection.aggregate([
-      {$match: where},
-      {
-        $lookup: {
-          from: 'songs',
-          localField: '_id',    // field in the current collection
-          foreignField: 'album',  // field in the from collection
-          as: 'songs'
-        }
-      },
-      {
-        $lookup: {
-          from: 'artists',
-          localField: 'songs.artists',    // field in the current collection
-          foreignField: '_id',  // field in the from collection
-          as: 'artists'
-        }
-      },
-      {$project: {'songs': 0, 'artists.picture': 0, 'artists._id': 0}}
-    ])
-
-    return await cursor.next()
-  }
-
+  /**
+   * Gets an Object with a document page and a count of all elements
+   * @param page
+   * @param pageSize
+   * @param where
+   * @returns {Object}
+   */
   async paginate (page, pageSize, where = {}) {
-    // Get all elements
     let response = {}
     this.collection.createIndex({name: 1})
     const cursor = this.collection.find(where, {_id: 0}).sort({name: 1})
@@ -58,42 +25,102 @@ class Album extends Base {
     return response
   }
 
-  async getAllSongs (_id) {
-    return await this.collection.aggregate([
-      {$match: {_id}},
-      {
-        $lookup: {
-          from: 'songs',
-          localField: '_id',
-          foreignField: 'artists',
-          as: 'songs'
-        }
-      },
+  /**
+   * Get the requested document
+   * @param where Object with the query
+   * @returns {Object}
+   */
+  async find (where) {
+    let response = await this.collection.findOne(where)
+
+    const cursor = this.collection.aggregate([
+      {$match: where},
+      {$lookup: {from: 'songs', localField: '_id', foreignField: 'album', as: 'songs'}},
       {$unwind: '$songs'},
-      {$replaceRoot: { newRoot: "$songs"}},
-      {
-        $lookup: {
-          from: 'albums',
-          localField: 'album',    // field in the current collection
-          foreignField: '_id',  // field in the from collection
-          as: 'album'
-        }
-      },
+      {$replaceRoot: {newRoot: '$songs'}},
+      {$lookup: {from: 'artists', localField: 'artists', foreignField: '_id', as: 'artists'}},
+      {$group: {_id: '$artists'}},
+      {$unwind: '$_id'},
+      {$group: {_id: '$_id'}},
+      {$replaceRoot: {newRoot: '$_id'}},
+    ])
+
+    response.artists = await cursor.toArray()
+      .then(result => (result) ? result : null)
+      .catch(error => console.error(error))
+
+    return response
+  }
+
+  /**
+   * Gets an Object with an Song document page and a count of all elements
+   * @param page
+   * @param pageSize
+   * @param where
+   * @returns Object
+   */
+  async songs (page, pageSize, where = {}) {
+    let response = {}
+    this.collection.createIndex({name: 1})
+
+    const cursor = this.collection.aggregate([
+      {$match: where},
+      {$lookup: {from: 'songs', localField: '_id', foreignField: 'album', as: 'songs'}},
+      {$unwind: '$songs'},
+      {$replaceRoot: {newRoot: '$songs'}},
+      {$lookup: {from: 'albums', localField: 'album', foreignField: '_id', as: 'album'}},
       {$unwind: '$album'},
-      {
-        $lookup: {
-          from: 'artists',
-          localField: 'artists',    // field in the current collection
-          foreignField: '_id',  // field in the from collection
-          as: 'artists'
-        }
-      },
+      {$lookup: {from: 'artists', localField: 'artists', foreignField: '_id', as: 'artists'}},
       {$project: {'album.picture': 0, 'album._id': 0, 'artists.picture': 0, 'artists._id': 0}},
-      {$addFields: {id: { $toString: "$_id" }}}
+      {$addFields: {id: {$toString: '$_id'}}},
+      {$sort: {title: 1}}
+    ])
+
+    response.count = (await cursor.toArray()).length
+    response.data = await cursor.skip(pageSize * (page - 1)).limit(pageSize).toArray()
+      .then(result => (result) ? result : null)
+      .catch(error => console.error(error))
+
+    return response
+  }
+
+  /**
+   * Gets an Array with all the songs related to the requested artist
+   * @param where
+   * @returns {Array}
+   */
+  async playAll (where) {
+    return await this.collection.aggregate([
+      {$match: where},
+      {$lookup: {from: 'songs', localField: '_id', foreignField: 'album', as: 'songs'}},
+      {$unwind: '$songs'},
+      {$replaceRoot: {newRoot: '$songs'}},
+      {$lookup: {from: 'albums', localField: 'album', foreignField: '_id', as: 'album'}},
+      {$unwind: '$album'},
+      {$lookup: {from: 'artists', localField: 'artists', foreignField: '_id', as: 'artists'}},
+      {$project: {'album.picture': 0, 'album._id': 0, 'artists.picture': 0, 'artists._id': 0}},
+      {$addFields: {id: {$toString: '$_id'}}},
+      {$sort: {title: 1}}
+    ]).toArray()
+      .then(result => (result) ? result : null)
+      .catch(error => console.error(error))
+  }
+
+  /**
+   * Search the query in the model and return all the result documents
+   * @param query
+   * @returns {Array}
+   */
+  async search (query) {
+    this.collection.createIndex({name: 'text'})
+    return await this.collection.aggregate([
+      {$match: {$text: {$search: query, $caseSensitive: false, $diacriticSensitive: false}}},
+      {$project: {_id: 0}},
+      {$sort: {name: 1}}
     ]).toArray()
       .then(result => (result) ? result : null)
       .catch(error => console.error(error))
   }
 }
 
-export default Album
+export default AlbumCollection
--- src/main/collections/songCollection.js
+++ src/main/collections/songCollection.js
@@ -1,31 +1,18 @@
 const Base = require('./_base').default
-const Mongo = require('../database').default
 
-class Song extends Base {
+class SongCollection extends Base {
   constructor () {
     super({model: 'songs'})
   };
 
   async get () {
     const cursor = this.collection.aggregate([
-      {
-        $lookup: {
-          from: 'albums',
-          localField: 'album',    // field in the current collection
-          foreignField: '_id',  // field in the from collection
-          as: 'album'
-        }
-      },
+      {$lookup: {from: 'albums', localField: 'album', foreignField: '_id', as: 'album'}},
       {$unwind: '$album'},
+      {$lookup: {from: 'artists', localField: 'artists', foreignField: '_id', as: 'artists'}},
       {
-        $lookup: {
-          from: 'artists',
-          localField: 'artists',    // field in the current collection
-          foreignField: '_id',  // field in the from collection
-          as: 'artists'
-        }
+        $project: {_id: {$toString: '$_id'}, 'album.picture': 0, 'album._id': 0, 'artists.picture': 0, 'artists._id': 0}
       },
-      {$project: {_id: {$toString: "$_id"},'album.picture': 0, 'album._id': 0, 'artists.picture': 0, 'artists._id': 0}},
     ])
 
     return await cursor.toArray()
@@ -37,28 +24,31 @@ class Song extends Base {
     this.collection.createIndex({title: 'text'})
     const cursor = this.collection.aggregate([
       {$match: {$text: {$search: query, $caseSensitive: false, $diacriticSensitive: false}}},
-      {
-        $lookup: {
-          from: 'albums',
-          localField: 'album',    // field in the current collection
-          foreignField: '_id',  // field in the from collection
-          as: 'album'
-        }
-      },
+      {$lookup: {from: 'albums', localField: 'album', foreignField: '_id', as: 'album'}},
       {$unwind: '$album'},
-      {
-        $lookup: {
-          from: 'artists',
-          localField: 'artists',    // field in the current collection
-          foreignField: '_id',  // field in the from collection
-          as: 'artists'
-        }
-      },
+      {$lookup: {from: 'artists', localField: 'artists', foreignField: '_id', as: 'artists'}},
       {$project: {'album.picture': 0, 'album._id': 0, 'artists.picture': 0, 'artists._id': 0}},
-      {$addFields: {id: { $toString: "$_id" }}}
+      {$addFields: {id: {$toString: '$_id'}}}
     ])
 
     return await cursor.toArray()
+      .then(result => (result) ? result : null)
+      .catch(error => console.error(error))
+  }
+
+  async searchOne (query) {
+    this.collection.createIndex({title: 'text'})
+    const cursor = this.collection.aggregate([
+      {$match: {$text: {$search: query, $caseSensitive: false, $diacriticSensitive: false}}},
+      {$limit: 1},
+      {$lookup: {from: 'albums', localField: 'album', foreignField: '_id', as: 'album'}},
+      {$unwind: '$album'},
+      {$lookup: {from: 'artists', localField: 'artists', foreignField: '_id', as: 'artists'}},
+      {$project: {'album.picture': 0, 'album._id': 0, 'artists.picture': 0, 'artists._id': 0}},
+      {$addFields: {id: {$toString: '$_id'}}}
+    ])
+
+    return await cursor.next()
       .then(result => (result) ? result : null)
       .catch(error => console.error(error))
   }
@@ -89,7 +79,7 @@ class Song extends Base {
         }
       },
       {$project: {'album.picture': 0, 'album._id': 0, 'artists.picture': 0, 'artists._id': 0}},
-      {$addFields: {id: { $toString: "$_id" }}}
+      {$addFields: {id: {$toString: '$_id'}}}
     ])
 
     response.data = await cursor.toArray()
@@ -100,82 +90,34 @@ class Song extends Base {
     return response
   }
 
-  async firstOrCreate (data) {
-    const mongo = new Mongo()
-    const db = await mongo.connect()
-    this.collection = db.collection(this.model)
-
-    return await this.collection.findOneAndUpdate(
-      {title: data.title},
-      {$setOnInsert: data},
-      {returnNewDocument: true, upsert: true})
-  }
-
-  async getWherePagination (page, pageSize, where = {}) {
-    let response = {}
-    this.collection.createIndex({title: 1})
-
-    const cursor = this.collection.aggregate([
-      {$match: where},
+  async playAll () {
+    return await this.collection.aggregate([
       {$sort: {title: 1}},
-      {$skip: pageSize * (page - 1)},
-      {$limit: pageSize},
-      {
-        $lookup: {
-          from: 'albums',
-          localField: 'album',    // field in the current collection
-          foreignField: '_id',  // field in the from collection
-          as: 'album'
-        }
-      },
+      {$lookup: {from: 'albums', localField: 'album', foreignField: '_id', as: 'album'}},
       {$unwind: '$album'},
-      {
-        $lookup: {
-          from: 'artists',
-          localField: 'artists',    // field in the current collection
-          foreignField: '_id',  // field in the from collection
-          as: 'artists'
-        }
-      },
+      {$lookup: {from: 'artists', localField: 'artists', foreignField: '_id', as: 'artists'}},
       {$project: {'album.picture': 0, 'album._id': 0, 'artists.picture': 0, 'artists._id': 0}},
-      {$addFields: {id: { $toString: "$_id" }}}
-    ])
-
-    response.data = await cursor.toArray()
+      {$addFields: {id: {$toString: '$_id'}}}
+    ]).toArray()
       .then(result => (result) ? result : null)
       .catch(error => console.error(error))
-
-    response.count = await this.collection.find(where).count()
-    return response
   }
 
-  async getAlbumsPaginate (page, pageSize, _id) {
-    let response = {}
-    this.collection.createIndex({title: 1})
-
-    const cursor = this.collection.aggregate([
-      {$match: {artists: _id}},
-      {$group: {_id: '$album'}},
-      {
-        $lookup: {
-          from: 'albums',
-          localField: '_id',    // field in the current collection
-          foreignField: '_id',  // field in the from collection
-          as: 'album'
-        }
-      },
+  /**
+   * Get the requested document
+   * @param where Object with the query
+   * @returns {Object}
+   */
+  async find (where) {
+    return await this.collection.aggregate([
+      {$match: where},
+      {$lookup: {from: 'albums', localField: 'album', foreignField: '_id', as: 'album'}},
       {$unwind: '$album'},
-      {$replaceRoot: {newRoot: '$album'}},
-      {$sort: {title: 1}}
-    ])
-
-    response.count = (await cursor.toArray()).length
-    response.data = await cursor.skip(pageSize * (page - 1)).limit(pageSize).toArray()
-      .then(result => (result) ? result : null)
-      .catch(error => console.error(error))
-
-    return response
+      {$lookup: {from: 'artists', localField: 'artists', foreignField: '_id', as: 'artists'}},
+      {$project: {'album.picture': 0, 'album._id': 0, 'artists.picture': 0, 'artists._id': 0}},
+      {$addFields: {id: {$toString: '$_id'}}}
+    ]).next()
   }
 }
 
-export default Song
+export default SongCollection
